a8ad383fb2d69a31259dcc8f1618704f8f6f24f6
sohail342 <sohailahmed34280@gmail.com>
2025-12-03 14:30:41 +0500

    chore: remove Irrelavant auth from health and root endpoints

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 """Main FastAPI application."""
 
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1.api import api_v1_router
 from app.api.v1.endpoints.custom_auth import verify_router
 from app.core.config import settings
-from app.core.users import current_active_verified_user
 
 
 def create_application() -> FastAPI:
@@ -38,7 +37,7 @@ def create_application() -> FastAPI:
 app = create_application()
 
 
-@app.get("/", dependencies=[Depends(current_active_verified_user)])
+@app.get("/")
 def root() -> dict:
     """Root endpoint."""
     return {
@@ -48,6 +47,6 @@ def root() -> dict:
     }
 
 
-@app.get("/health", dependencies=[Depends(current_active_verified_user)])
+@app.get("/health")
 async def health() -> dict:
     return {"status": "ok"}
